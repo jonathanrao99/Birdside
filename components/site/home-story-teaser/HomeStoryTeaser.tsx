856c1f7dcf3287486bf674c0d9a5d330223f897a
Jonathan Thota <jonathanrao5576@gmail.com>
2026-07-19 23:44:24 -0500

    Remove homepage detail CTA block

--- components/site/home-story-teaser/HomeStoryTeaser.tsx
+++ components/site/home-story-teaser/HomeStoryTeaser.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import { ORDER_NOW_URL } from "@/lib/site-shell-data";
 import HomeStoryMarquee from "./HomeStoryMarquee";
 import styles from "./home-story-teaser.module.css";
 
@@ -30,37 +28,6 @@ export default function HomeStoryTeaser() {
             <strong className={styles.hoursTime}>5 PM–12:30 AM</strong>
             <span className={styles.hoursNote}>Katy dine-in, pickup, and late-night runs.</span>
           </div>
-        </div>
-
-        <ul className={styles.factList} aria-label="Birdside details">
-          <li className={styles.factItem}>
-            <span className={styles.factNumber}>01</span>
-            <span className={styles.factText}>Fresh chicken cooked to order.</span>
-          </li>
-          <li className={styles.factItem}>
-            <span className={styles.factNumber}>02</span>
-            <span className={styles.factText}>100% Halal menu.</span>
-          </li>
-          <li className={styles.factItem}>
-            <span className={styles.factNumber}>03</span>
-            <span className={styles.factText}>Catering trays for groups.</span>
-          </li>
-          <li className={styles.factItem}>
-            <span className={styles.factNumber}>04</span>
-            <span className={styles.factText}>Pull up in Katy.</span>
-          </li>
-        </ul>
-
-        <div className={styles.ctaRow}>
-          <Link href="/menu" className={styles.ctaBtnPrimary}>
-            View menu
-          </Link>
-          <a href={ORDER_NOW_URL} className={styles.ctaBtn}>
-            Order now
-          </a>
-          <Link href="/catering" className={styles.inlineLink}>
-            Catering
-          </Link>
         </div>
       </div>
     </section>
